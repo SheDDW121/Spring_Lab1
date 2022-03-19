f093e3ae06aa127fac3aacbc1036bd0cb4ad45aa
SheDDW121 <90579357+SheDDW121@users.noreply.github.com>
2022-03-20 02:48:30 +0300

    Add files via upload

---
+++ WPF_APP/Converters.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_APP
+{
+    public class MultiConverters : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string Length, Start, End;
+                Length = values[0].ToString();
+                Start = values[1].ToString();
+                End = values[2].ToString();
+                return Length + ";" + Start + ";" + End;
+            }
+            catch (Exception ex)
+            {
+                return " ; ; ";
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string st = value.ToString();
+                string [] stt = st.Split(";", StringSplitOptions.RemoveEmptyEntries);
+                return new object[] { int.Parse(stt[0]), Double.Parse(stt[1]), Double.Parse(stt[2]) };
+            }
+            catch (Exception ex)
+            {
+                return new object[3];
+            }
+        }
+    }
+    public class Convert_Time : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double Time_HA, Time_EP, Time_NO_MKL;
+                Time_HA = Double.Parse(values[0].ToString());
+                Time_EP = Double.Parse(values[1].ToString());
+                Time_NO_MKL = Double.Parse(values[2].ToString());
+                return $"Time_HA = {Time_HA.ToString("F7")} ; Time_EP = {Time_EP.ToString("F7")}\nTime_NO_MKL = {Time_NO_MKL.ToString("F7")}\nHA / NO_MKL = {(Time_HA / Time_NO_MKL).ToString("F3")}" +
+                    $"\nEP / NO_MKL = {(Time_EP / Time_NO_MKL).ToString("F3")}";
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[3] { 0, 0, 0}; // doesn't matter what to return cause of One-Way binding
+        }
+    }
+
+    public class Convert_Accur : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double MaxDiff, Arg, Val_HA, Val_EP;
+                MaxDiff = Double.Parse(values[0].ToString());
+                Arg = Double.Parse(values[1].ToString());
+                Val_HA = Double.Parse(values[2].ToString());
+                Val_EP = Double.Parse(values[3].ToString());
+                return $"Max difference = {MaxDiff.ToString()}\nReaches with argument = {Arg.ToString()}\nValues in modes:\nHA = {Val_HA.ToString()}\nEP = {Val_EP.ToString()}";
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[3] { 0, 0, 0 }; // doesn't matter what to return cause of One-Way binding
+        }
+    }
+    public class Display_Zero : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double tmp = Double.Parse(value.ToString());
+                if (tmp == 0 || tmp == double.MaxValue)
+                    return "No data";
+                else return tmp.ToString("F3");
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
---
+++ WPF_APP/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using class_library;
+using Microsoft.Win32;
+
+namespace WPF_APP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        ViewData Item = new ViewData();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainGrid.DataContext = Item;
+            For_Times.DataContext = Item.BM.Time_Coll;
+            For_Accuracy.DataContext = Item.BM.Accur_Coll;
+            ForInput.DataContext = Item.Grid_V;
+            var enumDataSource = System.Enum.GetValues(typeof(System.Windows.HorizontalAlignment));
+        }
+
+        private void Add_Time_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Item.AddVMTime(new VMGrid(Item.Grid_V.Length, Item.Grid_V.Begin, Item.Grid_V.End), Item.MFun);
+                Item.WasChanged = true;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+        private void Add_Accur_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Item.AddVMAccur(new VMGrid(Item.Grid_V.Length, Item.Grid_V.Begin, Item.Grid_V.End), Item.MFun);
+                Item.WasChanged = true;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            if (Item.WasChanged)
+            {
+                var res = MessageBox.Show("Данные не были сохранены\nСохранить данные?", "Предупреждение", 
+                    MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (res == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        SaveFileDialog dialog = new SaveFileDialog();
+                        dialog.Filter = "Text Files (*.txt) | *.txt";
+                        dialog.ShowDialog();
+                        if (dialog.FileName != "")
+                        {
+                            Item.Save(dialog.FileName);
+                            Item.WasChanged = false;
+                        }
+                    }
+                    catch (Exception Ex)
+                    {
+                        MessageBox.Show(Ex.Message);
+                    }
+                    return;
+                }
+                Item.WasChanged = false;
+            }
+            try
+            {
+                Item = new ViewData();
+                MainGrid.DataContext = Item;
+                For_Times.DataContext = Item.BM.Time_Coll;
+                For_Accuracy.DataContext = Item.BM.Accur_Coll;
+                ForInput.DataContext = Item.Grid_V;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            if (Item.WasChanged)
+            {
+                var res = MessageBox.Show("Данные не были сохранены\nСохранить данные?", "Предупреждение", 
+                    MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (res == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        SaveFileDialog dialog = new SaveFileDialog();
+                        dialog.Filter = "Text Files (*.txt) | *.txt";
+                        dialog.ShowDialog();
+                        if (dialog.FileName != "")
+                        {
+                            Item.Save(dialog.FileName);
+                            Item.WasChanged = false;
+                        }
+                    }
+                    catch (Exception Ex)
+                    {
+                        MessageBox.Show(Ex.Message);
+                    }
+                    Item.WasChanged = false;
+                    return;
+                }
+            }
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.ShowDialog();
+                if (dialog.FileName != "")
+                {
+                    Item = new ViewData();
+                    Item.Load(dialog.FileName);
+                    MainGrid.DataContext = Item;
+                    For_Times.DataContext = Item.BM.Time_Coll;
+                    For_Accuracy.DataContext = Item.BM.Accur_Coll;
+                    ForInput.DataContext = Item.Grid_V;
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Text Files (*.txt) | *.txt" ;
+                dialog.ShowDialog();
+                if (dialog.FileName != "")
+                {
+                    Item.Save(dialog.FileName);
+                    Item.WasChanged = false;
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void Window_Closing(object sender, EventArgs e)
+        {
+            if (Item.WasChanged)
+            {
+                var res = MessageBox.Show("Данные не были сохранены\nСохранить данные?", "Предупреждение",
+                    MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (res == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        SaveFileDialog dialog = new SaveFileDialog();
+                        dialog.Filter = "Text Files (*.txt) | *.txt";
+                        dialog.ShowDialog();
+                        if (dialog.FileName != "")
+                        {
+                            Item.Save(dialog.FileName);
+                            Item.WasChanged = false;
+                        }
+                    }
+                    catch (Exception Ex)
+                    {
+                        MessageBox.Show(Ex.Message);
+                    }
+                    return;
+                }
+                Item.WasChanged = false;
+            }
+        }
+    }
+}
